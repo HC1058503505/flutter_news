ae6289d1e380cc27665ff6c86b7fa38a868e18f5
HC1058503505 <1058503505@qq.com>
2019-12-18 18:27:47 +0800

    完善头部轮播新闻

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:convert';
 import 'package:flutter_swiper/flutter_swiper.dart';
+
 class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -22,12 +23,12 @@ class _HomeState extends State<Home> {
 
   getNews() async {
     HttpClient httpClient = HttpClient();
-    Map<String , String> params = {
-      "appChannelId" : "1000",
-      "appVersion" : "5.6.5_1909240955",
-      "deviceModel" : "x86_64",
-      "deviceOsType" : "0",
-      "offset" : offset.toString()
+    Map<String, String> params = {
+      "appChannelId": "1000",
+      "appVersion": "5.6.5_1909240955",
+      "deviceModel": "x86_64",
+      "deviceOsType": "0",
+      "offset": offset.toString()
     };
     Uri newsUri = Uri.https('api.cgtn.com', '/app/api/news/home/list', params);
     var newsRequest = await httpClient.getUrl(newsUri);
@@ -35,35 +36,16 @@ class _HomeState extends State<Home> {
     var result = await newsResponse.transform(utf8.decoder).join();
     Map<String, dynamic> resultJSON = jsonDecode(result);
 
-    if(resultJSON["status"] != 200) {
+    if (resultJSON["status"] != 200) {
       return;
     }
 
     newsList = resultJSON["data"];
-    setState(() {
-    });
-
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-
-    // return CustomScrollView(
-    //     slivers: newsList?.map((el) {
-    //       var section = el as Map<String, dynamic>;
-    //       int styleType = int.parse(section["styleType"]);
-    //       List contents = section["contents"] as List;
-    //       switch (styleType) {
-    //         case 2:
-    //           return SliverToBoxAdapter(
-    //             child: swiperNews(contents),
-    //           );
-    //           break;
-    //         default:
-    //           return SliverToBoxAdapter();
-    //       }
-    //     })?.toList() ?? List<Widget>(),
-    // );
     // TODO: implement build
     return ListView.separated(
       itemCount: newsList?.length ?? 0,
@@ -89,24 +71,76 @@ class _HomeState extends State<Home> {
 
   Widget swiperNews(BuildContext context, List contents) {
     return Container(
-              child: Swiper(
-                      autoplay: true,
-                      loop: true,
-                      itemCount: contents.length,
-                      itemBuilder: (context, index) {
-                        Map<String, dynamic> item = contents[index] as Map<String, dynamic>;
-                        Map<String, dynamic> coverJSON = item["cover"] as Map<String, dynamic>;
-                        Map<String, dynamic> cover_16_9_JSON = coverJSON["r_16_9"] as Map<String, dynamic>;
-                        Map<String, dynamic> cover_16_9_quality_max_JSON = cover_16_9_JSON["quality_max"] as Map<String, dynamic>;
-                        String cover_16_9_quality_max_url = cover_16_9_quality_max_JSON["url"];
-                        return new Image.network(cover_16_9_quality_max_url,fit: BoxFit.fill,);
-                      },
-                      onTap: (index) {
-                        print(index);
-                      },
-                    ),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width * 9.0 / 16.0,
-            );
+      child: Swiper(
+        autoplay: true,
+        loop: true,
+        itemCount: contents.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> item = contents[index] as Map<String, dynamic>;
+          return swiperItem(item);
+        },
+        pagination: SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            color: Colors.grey
+          ),
+          alignment: Alignment.bottomRight
+        ),
+        onTap: (index) {
+          print(index);
+        },
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width * 9.0 / 16.0 + 130.0,
+    );
+  }
+
+  Widget swiperItem(Map<String, dynamic> item) {
+    String newsTitle = item["shortHeadline"] ?? item["longHeadline"] ?? "";
+    String timeStr = item["updateTime"] ?? item["publishTime"] ?? "";
+    DateTime dateTime =
+        DateTime.fromMillisecondsSinceEpoch(int.tryParse(timeStr) ?? 0);
+    String dateTimeStr = "${dateTime.year}-${dateTime.month}-${dateTime.day}";
+    String property = item["property"] ?? "";
+    Map<String, dynamic> coverJSON = item["cover"] as Map<String, dynamic>;
+    Map<String, dynamic> cover_16_9_JSON =
+        coverJSON["r_16_9"] as Map<String, dynamic>;
+    Map<String, dynamic> cover_16_9_quality_max_JSON =
+        cover_16_9_JSON["quality_max"] as Map<String, dynamic>;
+    String cover_16_9_quality_max_url = cover_16_9_quality_max_JSON["url"];
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          FadeInImage.assetNetwork(
+            placeholder: 'lib/images/placeholderimg.png',
+            image: cover_16_9_quality_max_url,
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              newsTitle,
+              maxLines: 2,
+              style: TextStyle(
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              dateTimeStr + '.' + property,
+              maxLines: 1,
+              style: TextStyle(
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 15),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
